eded174539fdd49eb826628de361a80194ac3b73
nsheetz <sheetz@gmail.com>
2016-09-23 16:11:52 -0500

    groundwork for efficiency-based perk permutation & misc bug fixes
    TrimpsSimulation now takes derived benefit factors based on perks, to
    allow us in the future to run simulations of large effect sizes without
    actually buying perks.
    -SimulationResult no longer includes perks (since they are not part of
    the simulation class any longer), Determinator will track perks
    separately
    -removed unnecessary vars from save/restore in pM (since they are
    static)
    -bug fixes and comments for derived drop/prod mods
    -added correction for storage costs based on packrat and building
    discount
    -improved min map time optimization to include map bonus from
    already-run maps (and separated it from minMaps == 0 since we might want
    to change the minMaps criteria someday)
    -added map cell drops to metal gain
    -added worker production while running maps to metal gain
    -standardized both optimizeMaps and !optimizeMaps to use runMap() (which
    accumulates map resources and buys stuff)
    -parameterized map size, turkimp mod, fraction of workers mining
    -removed minEquip since it is not actually used

--- src/PopulationManager.java
+++ src/PopulationManager.java
@@ -1,35 +1,32 @@
 
 public class PopulationManager {
-    private double resourcefulFactor;
+    private final double buildingDiscount;
     private double population;
     private double armyPopulation;
     private int unboughtCoordinations;
-    private double coordinationFactor;
-    private double carpentryFactor;
+    private final double coordFactor;
+    private final double popMod;
     private int gigaStations;
     private int warpStations;
     private double damageFactor;
     private double metal;
     
-    private double SVresourcefulFactor;
     private double SVpopulation;
     private double SVarmyPopulation;
     private int SVunboughtCoordinations;
-    private double SVcoordinationFactor;
-    private double SVcarpentryFactor;
     private int SVgigaStations;
     private int SVwarpStations;
     private double SVdamageFactor;
     private double SVmetal;
 
-    public PopulationManager(final int carpentryLevel,
-            final int carpentry2Level, final int resourcefulLevel,
-            final int coordinatedLevel) {
-        resourcefulFactor = Math.pow(0.95, resourcefulLevel);
-        coordinationFactor = 1 + 0.25 * Math.pow(0.98, coordinatedLevel);
-        carpentryFactor = Math.pow(1.1, carpentryLevel)
-                * (1 + 0.0025 * carpentry2Level);
-        population = 100000 * carpentryFactor;
+    public PopulationManager(
+    		final double popMod,
+    		final double buildingDiscount,
+    		final double coordFactor) {
+        this.buildingDiscount = buildingDiscount;
+        this.coordFactor = coordFactor;
+        this.popMod = popMod;
+        population = 100000 * popMod;
         unboughtCoordinations = 0;
         armyPopulation = 1;
         gigaStations = -1;
@@ -39,12 +36,9 @@ public class PopulationManager {
     }
     
     public void save() {
-        SVresourcefulFactor = resourcefulFactor;
         SVpopulation = population;
         SVarmyPopulation = armyPopulation;
         SVunboughtCoordinations = unboughtCoordinations;
-        SVcoordinationFactor = coordinationFactor;
-        SVcarpentryFactor = carpentryFactor;
         SVgigaStations = gigaStations;
         SVwarpStations = warpStations;
         SVdamageFactor = damageFactor;
@@ -52,12 +46,9 @@ public class PopulationManager {
     }    
     
     public void restore() {
-        resourcefulFactor = SVresourcefulFactor;
         population = SVpopulation;
         armyPopulation = SVarmyPopulation;
         unboughtCoordinations = SVunboughtCoordinations;
-        coordinationFactor = SVcoordinationFactor;
-        carpentryFactor = SVcarpentryFactor;
         gigaStations = SVgigaStations;
         warpStations = SVwarpStations;
         damageFactor = SVdamageFactor;
@@ -74,7 +65,7 @@ public class PopulationManager {
         if (unboughtCoordinations == 0 || !canBuyCoordination()) {
             return false;
         }
-        armyPopulation *= coordinationFactor;
+        armyPopulation *= coordFactor;
         armyPopulation = Math.ceil(armyPopulation);
         damageFactor *= 1.25;
         damageFactor = Math.ceil(damageFactor);
@@ -88,11 +79,11 @@ public class PopulationManager {
 
     private double getWarpStationCost() {
         return 1000000000000000l * Math.pow(1.75, gigaStations)
-                * Math.pow(1.4, warpStations) * resourcefulFactor;
+                * Math.pow(1.4, warpStations) * buildingDiscount;
     }
 
     private double getWarpStationEffect() {
-        return 10000 * Math.pow(1.2, gigaStations) * carpentryFactor;
+        return 10000 * Math.pow(1.2, gigaStations) * popMod;
     }
 
     private void buyWarpStation() {
--- src/SimulationResult.java
+++ src/SimulationResult.java
@@ -2,14 +2,12 @@
 public class SimulationResult {
      public final double helium;
      public final double hours;
-     public final Perks perks;
      public final int zone;
      
 
-     public SimulationResult(final double helium, final double hours, final Perks perks, final int zone){
+     public SimulationResult(final double helium, final double hours, final int zone){
          this.helium = helium;
          this.hours = hours;
-         this.perks = perks;
          this.zone = zone;
      }
 }
